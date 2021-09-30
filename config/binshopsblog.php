b0f7a96c0357e44fe3e6ab5427a29a2908df9328
Daan Asscheman <dasscheman@gmail.com>
2021-09-30 09:44:17 +0200

    test

--- config/binshopsblog.php
+++ config/binshopsblog.php
@@ -188,5 +188,5 @@ return [
     ],
 
     //Shows full text of post in listing pages like search result page or category page. Now it shows a preview
-    'show_full_text_at_list' => false,
+    'show_full_text_at_list' => true,
 ];
